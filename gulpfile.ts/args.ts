b62a29ca6e416527bbab269c55391f7938b005a0
Nils <nils.yseboodt@gmail.com>
2024-01-15 23:27:52 +0100

    Updated to v1.0.1

--- gulpfile.ts/args.ts
+++ gulpfile.ts/args.ts
@@ -24,7 +24,8 @@ class Args {
       git.getLatestVersionTag(),
       foundryManifest.getManifest().file.version,
     ]);
-    const latestVersion = versions.filter(v => !!v).sort(Args.sortVersions)[versions.length - 1];
+    console.log(versions)
+    const latestVersion = versions.filter(v => !!v).sort(this.sortVersions)[versions.length - 1];
     if (latestVersion.startsWith('v')) {
       return latestVersion.substring(1)
     }
@@ -103,7 +104,7 @@ class Args {
     return null;
   }
 
-  private static sortVersions(a: string, b: string): number {
+  public sortVersions(a: string, b: string): number {
     const aRgx = Args.versionMatch.exec(a);
     const bRgx  = Args.versionMatch.exec(b);
 
